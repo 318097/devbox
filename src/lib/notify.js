1a8edde9cd2e6d5e7428f35e29a98452f606388e
Mehul Lakhanpal <318097@gmail.com>
2022-06-17 19:49:41 +0530

    Refactor: Switched to ant 'message'

--- src/lib/notify.js
+++ src/lib/notify.js
@@ -1,7 +1,14 @@
-import { StatusBar } from "@codedrops/react-ui";
+import { message } from "antd";
+
+message.config({
+  getContainer: () => {
+    const iframe = document.querySelector("#extension-root iframe#iframe-root");
+    return iframe?.contentWindow.document.body.querySelector(".react-ui");
+  },
+});
 
 const notify = (msg, type = "success") => {
-  setTimeout(() => StatusBar.notify(msg, { type }), 300);
+  message.info(msg);
 };
 
 export default notify;
